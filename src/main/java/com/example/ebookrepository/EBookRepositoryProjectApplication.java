5a85d2cef6e486b9ea940d0bbd04fafa6f5fbdfe
Marko Jokic <markojokic1996@gmail.com>
2017-12-10 10:39:05 +0100

    Hello World

---
+++ src/main/java/com/example/ebookrepository/EBookRepositoryProjectApplication.java
@@ -0,0 +1,12 @@
+package com.example.ebookrepository;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EBookRepositoryProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EBookRepositoryProjectApplication.class, args);
+	}
+}
