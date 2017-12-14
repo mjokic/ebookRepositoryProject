d99755730d586a722c05bcf4153556862cdca699
Marko Jokic <markojokic1996@gmail.com>
2017-12-14 18:28:10 +0100

    On table item click added

--- src/main/resources/static/js/admin.js
+++ src/main/resources/static/js/admin.js
@@ -1,4 +1,107 @@
-$.getScript("js/books.js", function() {
-    alert("Script loaded but not necessarily executed.");
+$(document).ready(function () {
 
+    loadCategories();
+
+});
+
+
+function generatePanelCategories(categories) {
+    categories.forEach(function (category) {
+        var categoryId = category['id'];
+        var category_name = category['name'];
+
+        var collapseId = 'collapse' + categoryId;
+
+        var panel = '<div class="panel-group col-lg-4 col-md-6">' +
+            '            <div id="pan' + categoryId + '" class="panel panel-default">' +
+            '                <!-- Default panel contents -->' +
+            '                <div id="' + categoryId + '" onclick="onCategoryClick(this.id)" class="panel-heading clearfix" data-toggle="collapse" href="#' + collapseId + '">' +
+            '                    <h4 class="panel-title pull-left">' +
+            '                        ' + category_name +
+            '                    </h4>' +
+            '                    <div class="pull-right">' +
+            '                       <span class="glyphicon glyphicon-plus-sign"></span>' +
+            '                       <span class="glyphicon glyphicon-minus-sign"></span> ' +
+            '                       <span class="glyphicon glyphicon-edit"></span> ' +
+            '                    </div>' +
+            '                </div>' +
+            '                <div id="' + collapseId + '" class="panel-collapse collapse">' +
+            '                    <div class="panel-body">' +
+            '                        <table id="table' + categoryId + '" class="table table-striped">' +
+            '' +
+            '                        </table>' +
+            '                    </div>' +
+            '                </div>' +
+            '            </div>' +
+            '        </div>';
+
+        $('#main').append(panel);
+
+    })
+}
+
+
+function generateTable(category_id, books) {
+    var rows = '';
+
+    books.forEach(function (book) {
+        rows += '' +
+            '                <tr class="clickable-row">' +
+            '                    <td class="filterable-cell">' + book.id + '</td>' +
+            '                    <td class="filterable-cell">' + book.title + '</td>' +
+            '                    <td class="filterable-cell">' + book.author + '</td>' +
+            '                    <td class="filterable-cell">' + book.publicationYear + '</td>' +
+            '                </tr>';
+    });
+
+    return '<table id="table' + category_id + '" class="table table-striped">' +
+        '<thead>' +
+        '                <tr>' +
+        '                    <th>Id</th>' +
+        '                    <th>Title</th>' +
+        '                    <th>Author</th>' +
+        '                    <th>Year</th>' +
+        '                </tr>' +
+        '            </thead>' +
+        '            <tbody>' +
+        rows +
+        '            </tbody>' +
+        '        </table>';
+}
+
+
+function onCategoryClick(category_id) {
+    loadBooks(category_id);
+}
+
+
+function loadCategories() {
+    $.ajax({
+        type: "GET",
+        url: "/category",
+        dataType: "json",
+        success: function (data) {
+            generatePanelCategories(data);
+        }
+    });
+}
+
+function loadBooks(category_id) {
+    $.ajax({
+        type: "GET",
+        url: "/ebook/category/" + category_id,
+        dataType: "json",
+        success: function (books) {
+
+            var myTable = generateTable(category_id, books);
+
+            $('#table' + category_id).replaceWith(myTable);
+
+        }
+    });
+}
+
+$('body').on('click', 'tr.clickable-row', function() {
+    var ohShit = $(this).children("td.filterable-cell").first().text();
+    alert(ohShit);
 });
--- src/main/resources/static/js/books.js
+++ src/main/resources/static/js/books.js
@@ -12,22 +12,22 @@ function generatePanelCategories(categories) {
 
         var collapseId = 'collapse' + categoryId;
 
-        var panel = '<div class="panel-group col-lg-4 col-md-6">'+
-            '            <div id="pan'+categoryId+'" class="panel panel-default">'+
-            '                <!-- Default panel contents -->'+
-            '                <div id="'+categoryId+'" onclick="onCategoryClick(this.id)" class="panel-heading" data-toggle="collapse" href="#'+collapseId+'">'+
-            '                    <h4 class="panel-title">'+
-            '                        '+ category_name +
-            '                    </h4>'+
-            '                </div>'+
-            '                <div id="'+collapseId+'" class="panel-collapse collapse">'+
-            '                    <div class="panel-body">'+
-            '                        <table id="table'+categoryId+'" class="table table-striped">'+
-            ''+
-            '                        </table>'+
-            '                    </div>'+
-            '                </div>'+
-            '            </div>'+
+        var panel = '<div class="panel-group col-lg-4 col-md-6">' +
+            '            <div id="pan' + categoryId + '" class="panel panel-default">' +
+            '                <!-- Default panel contents -->' +
+            '                <div id="' + categoryId + '" onclick="onCategoryClick(this.id)" class="panel-heading clearfix" data-toggle="collapse" href="#' + collapseId + '">' +
+            '                    <h4 class="panel-title pull-left">' +
+            '                        ' + category_name +
+            '                    </h4>' +
+            '                </div>' +
+            '                <div id="' + collapseId + '" class="panel-collapse collapse">' +
+            '                    <div class="panel-body">' +
+            '                        <table id="table' + categoryId + '" class="table table-striped">' +
+            '' +
+            '                        </table>' +
+            '                    </div>' +
+            '                </div>' +
+            '            </div>' +
             '        </div>';
 
         $('#main').append(panel);
@@ -41,38 +41,32 @@ function generateTable(category_id, books) {
 
     books.forEach(function (book) {
         rows += '' +
-            '                <tr>' +
-            '                    <td class="filterable-cell">'+book.id+'</td>' +
-            '                    <td class="filterable-cell">'+book.title+'</td>' +
-            '                    <td class="filterable-cell">'+book.author+'</td>' +
-            '                    <td class="filterable-cell">'+book.publicationYear+'</td>' +
+            '                <tr class="clickable-row">' +
+            '                    <td class="filterable-cell">' + book.id + '</td>' +
+            '                    <td class="filterable-cell">' + book.title + '</td>' +
+            '                    <td class="filterable-cell">' + book.author + '</td>' +
+            '                    <td class="filterable-cell">' + book.publicationYear + '</td>' +
             '                </tr>';
     });
 
-    return '<table id="table'+category_id+'" class="table table-striped">'+
-                '<thead>'+
-        '                <tr>'+
-        '                    <th>Id</th>'+
-        '                    <th>Title</th>'+
-        '                    <th>Author</th>'+
-        '                    <th>Year</th>'+
-        '                </tr>'+
-        '            </thead>'+
-        '            <tbody>'+
-                        rows +
-        '            </tbody>'+
+    return '<table id="table' + category_id + '" class="table table-striped">' +
+        '<thead>' +
+        '                <tr>' +
+        '                    <th>Id</th>' +
+        '                    <th>Title</th>' +
+        '                    <th>Author</th>' +
+        '                    <th>Year</th>' +
+        '                </tr>' +
+        '            </thead>' +
+        '            <tbody>' +
+        rows +
+        '            </tbody>' +
         '        </table>';
 }
 
 
 function onCategoryClick(category_id) {
-
     loadBooks(category_id);
-
-    // $('.panel').on('shown.bs.collapse', function (e) {
-    //     alert("CALLED!" + e.currentTarget.id);
-    // });
-
 }
 
 
@@ -93,12 +87,13 @@ function loadBooks(category_id) {
         url: "/ebook/category/" + category_id,
         dataType: "json",
         success: function (books) {
-
             var myTable = generateTable(category_id, books);
-
-            $('#table'+category_id).replaceWith(myTable);
-
+            $('#table' + category_id).replaceWith(myTable);
         }
     });
 }
 
+$('body').on('click', 'tr.clickable-row', function () {
+    var ohShit = $(this).children("td.filterable-cell").first().text();
+    alert(ohShit);
+});
