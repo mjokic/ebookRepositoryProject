8ec405c474ea8d4567594850edd282c2189d9781
Marko Jokic <markojokic1996@gmail.com>
2017-12-21 18:55:18 +0100

    Search on admin panel

--- src/main/resources/static/js/admin.js
+++ src/main/resources/static/js/admin.js
@@ -67,6 +67,35 @@ function generatePanelCategories(categories) {
     })
 }
 
+function generatePanelCategoriesForSearch(categories) {
+    categories.forEach(function (category) {
+        var categoryId = category['id'];
+        var category_name = category['name'];
+
+        var collapseId = 'collapse' + categoryId;
+
+        var panel = '<div class="panel-group col-lg-4 col-md-6">' +
+            '            <div id="pan' + categoryId + '" class="panel panel-default">' +
+            '                <!-- Default panel contents -->' +
+            '                <div id="' + categoryId + '" class="panel-heading clearfix" data-toggle="collapse" href="#' + collapseId + '">' +
+            '                    <h4 class="panel-title pull-left">' +
+            '                        ' + category_name +
+            '                    </h4>' +
+            '                </div>' +
+            '                <div id="' + collapseId + '" class="panel-collapse collapse">' +
+            '                    <div class="panel-body">' +
+            '                        <table id="table' + categoryId + '" class="table table-striped">' +
+            '' +
+            '                        </table>' +
+            '                    </div>' +
+            '                </div>' +
+            '            </div>' +
+            '        </div>';
+
+        $('#main').append(panel);
+
+    })
+}
 
 function generateTable(category_id, books) {
     var rows = '';
@@ -439,7 +468,117 @@ $('#delete_ebook').click(function () {
 });
 
 
+$('#search_form').submit(function (e) {
+    e.preventDefault();
+
+    var title = $('#title').val();
+    var titleCb = $('#titleCb').prop('checked');
+
+    var author = $('#author').val();
+    var authorCb = $('#authorCb').prop('checked');
+
+    var keywords = $('#keywords').val();
+    var keywordsCb = $('#keywordsCb').prop('checked');
+
+    var content = $('#content').val();
+    var contentCb = $('#contentCb').prop('checked');
+
+    var language = $('#language').val();
+    var languageCb = $('#languageCb').prop('checked');
+
+    var type = $("input[name='searchType']:checked").val();
+
+    var data = {
+        "title": {
+            "value": title,
+            "searchType": titleCb
+        },
+        "author": {
+            "value": author,
+            "searchType": authorCb
+        },
+        "keywords": {
+            "value": keywords,
+            "searchType": keywordsCb
+        },
+        "content": {
+            "value": content,
+            "searchType": contentCb
+        },
+        "language": {
+            "value": language,
+            "searchType": languageCb
+        },
+        "type": type
+    };
+
+    $.ajax({
+        type: $(this).attr("method"),
+        url: $(this).attr("action"),
+        data: JSON.stringify(data),
+        contentType: "application/json",
+        success: function (response) {
+            $('.panel-group').remove(); // removing old results
+
+            var categories = extract_categories(response);
+            generatePanelCategoriesForSearch(categories);
+
+            var lista = groupBy(response);
+
+            lista.forEach(function (books) {
+                var category_id = books[0]['category']['id'];
+                var myTable = generateTable(category_id, books);
+                $('#table' + category_id).replaceWith(myTable);
+            })
+
+            $('#modalSearch').modal('toggle');
+        },
+        error: function (err) {
+            var json = err.responseJSON;
+            alert(json['message']);
+        }
+    });
+
+});
+
 function refresh() {
     $('.panel-group').remove(); // removing old results
     loadCategories(true, 0);
 }
+
+function extract_categories(ebooks) {
+    var categories = {};
+
+    ebooks.forEach(function (ebook) {
+        var category = ebook['category'];
+        categories[category['id']] = category;
+    });
+
+
+    var result = [];
+    for (var key in categories) {
+        result.push(categories[key]);
+    }
+
+    return result;
+}
+
+function groupBy(collection) {
+    var i = 0, val, index, values = [], result = [];
+
+    for (; i < collection.length; i++) {
+        val = collection[i]['category']['id'];
+        index = values.indexOf(val);
+        if (index > -1)
+            result[index].push(collection[i]);
+        else {
+            values.push(val);
+            result.push([collection[i]]);
+        }
+    }
+    return result;
+}
+
+$('#modalSearch').on('hidden.bs.modal', function () {
+    $(this).find("input[type=text]").val('').end();
+});
