a189f833407c3c74e77a6842190c0cc3f3ffa1dc
Marko Jokic <markojokic1996@gmail.com>
2017-12-22 11:42:07 +0100

    Language backend finished

--- src/main/java/com/example/ebookrepository/controller/LanguageController.java
+++ src/main/java/com/example/ebookrepository/controller/LanguageController.java
@@ -1,6 +1,7 @@
 package com.example.ebookrepository.controller;
 
 import com.example.ebookrepository.model.Language;
+import com.example.ebookrepository.model.Status;
 import com.example.ebookrepository.service.LanguageService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
@@ -29,8 +30,31 @@ public class LanguageController {
 
     @PutMapping
     public ResponseEntity<?> addLanguage(@RequestBody Language language){
+        Language newLanguage = new Language();
+        newLanguage.setName(language.getName());
+
+        languageService.addEditLanguage(newLanguage);
+        return new ResponseEntity<>(
+                new Status(true, "Language successfully added!"),
+                HttpStatus.OK);
+    }
+
+
+    @PostMapping
+    public ResponseEntity<?> editLanguage(@RequestBody Language language){
         languageService.addEditLanguage(language);
-        return new ResponseEntity<>(HttpStatus.OK);
+        return new ResponseEntity<>(
+                new Status(true, "Language successfully edited!"),
+                HttpStatus.OK);
+    }
+
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<?> deleteLanguage(@PathVariable("id") int id){
+        languageService.deleteLanguage(id);
+        return new ResponseEntity<>(
+                new Status(true, "Language successfully deleted!"),
+                HttpStatus.OK);
     }
 
 }
